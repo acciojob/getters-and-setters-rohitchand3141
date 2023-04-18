af97ecefc14f15e72d3c45674096afdb76a70590
ROHIT CHAND <100352296+rohitchand3141@users.noreply.github.com>
2023-04-18 20:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor (name,age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+	get name(name)
+	{
+		return this.name;
+	}
+	set age(age)
+	{
+		
+	}
+}
+class Student extends Person
+{
+	study(name)
+	{
+		console.log(name+"is studing");
+	}
+} 
+class Teacher extends Person{
+	teach()
+	{
+		console.log(name+"is teaching");
+	}
+}
 
 class Student extends Person {}
 
